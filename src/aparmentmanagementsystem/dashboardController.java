936d04a4ae8b417760765fff6c42c4fdcbee2a3b
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2023-10-31 22:30:43 +0700

    Them hanh dong logout, minimize

---
+++ src/aparmentmanagementsystem/dashboardController.java
@@ -0,0 +1,106 @@
+package aparmentmanagementsystem;
+
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class dashboardController implements Initializable{
+    
+    @FXML
+    private AnchorPane main_form;
+    
+    @FXML
+    private Button close;
+    
+    @FXML
+    private Button logout;
+    
+    @FXML
+    private Button minimize;
+    
+    @FXML
+    private Label username;
+    
+    @FXML
+    private Button dashboard_btn;
+    
+    
+    private double x = 0;
+    private double y = 0;
+    
+    public void logout() {
+        try{
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Massage");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to logout?");
+            Optional<ButtonType> option = alert.showAndWait();
+            
+            if(option.get().equals(ButtonType.OK)){
+                
+                logout.getScene().getWindow().hide();
+                
+                Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+                Stage stage = new Stage();
+                Scene scene = new Scene(root);
+                
+                root.setOnMousePressed((MouseEvent event) -> {
+                    x = event.getSceneX();
+                    y = event.getSceneY();
+                });
+        
+            root.setOnMouseDragged((MouseEvent event) -> {
+                stage.setX(event.getScreenX() - x);
+                stage.setY(event.getScreenY() - y);
+            
+                stage.setOpacity(.8f);
+            });
+        
+            root.setOnMouseReleased((MouseEvent event) -> {
+                stage.setOpacity(1);
+            });
+        
+            stage.initStyle(StageStyle.TRANSPARENT);
+                
+            stage.setScene(scene);
+            stage.show();
+            }
+        }catch(Exception e){e.printStackTrace();}
+    }
+    
+    public void displayUsername(){
+        String user = data.username;
+        user = user.substring(0, 1).toUpperCase() + user.substring(1);
+        username.setText(user);
+    }
+    
+    public void close() {
+        System.exit(0);
+    }
+    
+    public void minimize() {
+        Stage stage = (Stage)minimize.getScene().getWindow();
+        stage.setIconified(true);
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        displayUsername();
+    }
+    
+}
